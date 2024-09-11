2eaf9ddcb8bcbafaca11beb1fc78474d0161322e
eli-stewart <eliwstewart10@gmail.com>
2024-09-11 16:25:43 -0700

    api

---
+++ team_manager_api/members/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import TeamMember
+
+class TeamMemberForm(forms.ModelForm):
+    class Meta:
+        model = TeamMember
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'role']
